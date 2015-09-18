bd99a4382f051817f6e0eebc2fff6cc43afbca02
Mathieu David <mathieudavid@mathieudavid.org>
2015-09-18 22:13:55 +0200

    Converted all css to stylus

--- README.md
+++ README.md
@@ -12,12 +12,12 @@ To have an idea of what a rendered book looks like,take a look at the [**Documen
 
 ## Installation
 
-Before building mdBook, make sure that you installed `stylus` from `npm` because it is used to compile the stylesheets
+Before building mdBook, make sure that you installed `stylus` and `nib` from `npm` because it is used to compile the stylesheets
 
 Install [node.js](https://nodejs.org/en/)
 
 ```
-npm install -g stylus
+npm install -g stylus nib
 ```
 
 ```
--- build.rs
+++ build.rs
@@ -10,12 +10,17 @@ fn main() {
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
     let theme_dir = Path::new(&manifest_dir).join("src/theme/");
-    let stylus_dir = theme_dir.join("stylus");
+    let stylus_dir = theme_dir.join("stylus/book.styl");
 
-    Command::new("stylus")
-        .arg(format!("{}", stylus_dir.to_str().unwrap()))
-        .arg("--out")
-        .arg(format!("{}", theme_dir.to_str().unwrap()))
-        .status().unwrap();
+    if !Command::new("stylus")
+            .arg(format!("{}", stylus_dir.to_str().unwrap()))
+            .arg("--out")
+            .arg(format!("{}", theme_dir.to_str().unwrap()))
+            .arg("--use")
+            .arg("nib")
+            .status().unwrap()
+            .success() {
+        panic!("Stylus encoutered an error");
+    }
 
 }
