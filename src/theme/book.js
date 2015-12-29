71213f40dafdfe70da8b49e33fd9bd5c2ee79712
Mathieu David <mathieudavid@mathieudavid.org>
2015-12-29 13:08:25 +0100

    Add expand/collapse button to show and hide the hidden code lines

--- src/theme/book.js
+++ src/theme/book.js
@@ -118,12 +118,21 @@ $( document ).ready(function() {
     var hiding_character = "#";
 
     $("code.language-rust").each(function(i, block){
+
+        // hide lines
         var lines = $(this).html().split("\n");
         var first_non_hidden_line = false;
+        var lines_hidden = false;
 
         for(var n = 0; n < lines.length; n++){
             if($.trim(lines[n])[0] == hiding_character){
-                lines[n] = "<span class=\"hidden\">" + lines[n] + "</span>";
+                if(first_non_hidden_line){
+                    lines[n] = "<span class=\"hidden\">" + "\n" + lines[n].substr(1) + "</span>";
+                }
+                else {
+                    lines[n] = "<span class=\"hidden\">" + lines[n].substr(1) + "\n"  +  "</span>";
+                }
+                lines_hidden = true;
             }
             else if(first_non_hidden_line) {
                 lines[n] = "\n" + lines[n];
@@ -133,6 +142,23 @@ $( document ).ready(function() {
             }
         }
         $(this).html(lines.join(""));
+
+        // If no lines were hidden, return
+        if(!lines_hidden) { return; }
+
+        // add expand button
+        $(this).parent().prepend("<i class=\"fa fa-expand\"></i>");
+
+        $(this).parent().find("i").click(function(e){
+            if( $(this).hasClass("fa-expand") ) {
+                $(this).removeClass("fa-expand").addClass("fa-compress");
+                $(this).parent().find("span.hidden").removeClass("hidden").addClass("unhidden");
+            }
+            else {
+                $(this).removeClass("fa-compress").addClass("fa-expand");
+                $(this).parent().find("span.unhidden").removeClass("unhidden").addClass("hidden");
+            }
+        });
     });
 
 
