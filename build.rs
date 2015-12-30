1d3f526273ebc39ea27db2d028109ae1130f1322
Pascal Hertleif <killercup@gmail.com>
2015-12-30 12:59:19 +0100

    Switch from Stylus to SASS, Add sass-rs to build

--- build.rs
+++ build.rs
@@ -1,29 +1,26 @@
 // build.rs
+extern crate sass_rs;
 
-use std::process::Command;
 use std::env;
 use std::path::Path;
+use std::fs::File;
+use std::io::Write;
+
+use sass_rs::sass_context::SassFileContext;
 
 fn main() {
-
     if let Ok(_) = env::var("CARGO_FEATURE_REGENERATE_CSS") {
-
         // Compile stylus stylesheet to css
         let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
         let theme_dir = Path::new(&manifest_dir).join("src/theme/");
-        let stylus_dir = theme_dir.join("stylus/book.styl");
+        let scss_index_file = theme_dir.join("stylus/book.scss");
 
-        if !Command::new("stylus")
-                .arg(format!("{}", stylus_dir.to_str().unwrap()))
-                .arg("--out")
-                .arg(format!("{}", theme_dir.to_str().unwrap()))
-                .arg("--use")
-                .arg("nib")
-                .status().unwrap()
-                .success() {
-            panic!("Stylus encoutered an error");
-        }
+        let css = SassFileContext::new(&scss_index_file.to_string_lossy())
+                                 .compile()
+                                 .expect("error compiling SCSS files");
+        let mut output_file = File::create(&theme_dir.join("book.css"))
+                                  .expect("error creating CSS file");
+        output_file.write_all(&mut css.as_bytes()).expect("error writing CSS to file");
     }
-
 }
