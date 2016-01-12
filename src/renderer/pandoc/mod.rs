7da175bba308626165fd2d4a387afcd56c66e35c
Adam Solove <asolove@gmail.com>
2016-01-11 21:19:22 -0500

    Get book contents and pass to pandoc for epub version.

--- src/renderer/pandoc/mod.rs
+++ src/renderer/pandoc/mod.rs
@@ -3,34 +3,40 @@ use book::MDBook;
 use book::bookitem::BookItem;
 
 use std::error::Error;
+use std::process::Command;
 
-enum PandocOutput {
-    Epub,
-    Pdf
-}
-
-pub struct Pandoc {
-    output: PandocOutput
-}
+pub struct Pandoc;
 
 impl Pandoc {
   pub fn new() -> Pandoc {
-    Pandoc { output: PandocOutput::Epub }
+    Pandoc
   }
 }
 
 impl Renderer for Pandoc {
     fn render(&self, book: &MDBook) -> Result<(), Box<Error>> {
+        let mut paths = vec!();
 
         for item in book.iter() {
             match *item {
-                BookItem::Chapter(ref title, _) => {
-                    println!("{}", title)
+                BookItem::Chapter(_, ref ch) => {
+                    paths.push(book.get_src().join(&ch.path).into_os_string());
                 },
-                _ => println!("Something I don't understand")
+                _ => println!("FIXME: don't understand this kind of BookItem")
             }
         }
 
-        Ok(())
+        let output = Command::new("pandoc")
+            .arg("-S")
+            .arg("-osample.epub")
+            .args(&paths)
+            .output();
+
+        match output {
+            Ok(_) => Ok(()),
+            Err(e) => Err(Box::new(e))
+        }
+        // FIXME: why doesn't this work
+        // output.map(|_| ()).map_err(|e| Box::new(e))
     }
 }
