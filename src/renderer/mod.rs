6bc088db6ef70016ccf80c2d98f8ebc74ca0828f
Mathieu David <mathieudavid@mathieudavid.org>
2016-04-27 14:19:59 +0200

    (Refactor) Move the Render trait into mod.rs instead of submodule

--- src/renderer/mod.rs
+++ src/renderer/mod.rs
@@ -1,5 +1,9 @@
-pub use self::renderer::Renderer;
 pub use self::html_handlebars::HtmlHandlebars;
 
-pub mod renderer;
 mod html_handlebars;
+
+use std::error::Error;
+
+pub trait Renderer {
+    fn render(&self, book: &::book::MDBook) -> Result<(), Box<Error>>;
+}
--- src/renderer/renderer.rs
+++
@@ -1,5 +0,0 @@
-use std::error::Error;
-
-pub trait Renderer {
-    fn render(&self, book: &::book::MDBook) -> Result<(), Box<Error>>;
-}
