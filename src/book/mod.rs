efc2644f84469bb5c04d77b4356db74fe1bf065d
Mathieu David <mathieudavid@mathieudavid.org>
2016-06-28 22:54:26 +0200

    Include the new Book struct in a hashmap alongside the old representation

--- src/book/mod.rs
+++ src/book/mod.rs
@@ -18,12 +18,13 @@ use std::io;
 use std::io::Write;
 use std::io::ErrorKind;
 use std::process::Command;
+use std::collections::HashMap;
 
 use {theme, parse, utils};
 use renderer::{Renderer, HtmlHandlebars};
 
 
-pub struct MDBook {
+pub struct MDBook<'a> {
     root: PathBuf,
     dest: PathBuf,
     src: PathBuf,
@@ -34,12 +35,13 @@ pub struct MDBook {
     pub description: String,
 
     pub content: Vec<BookItem>,
+    books: HashMap<&'a str, Book>,
     renderer: Box<Renderer>,
 
     livereload: Option<String>,
 }
 
-impl MDBook {
+impl<'a> MDBook<'a> {
     /// Create a new `MDBook` struct with root directory `root`
     ///
     /// Default directory paths:
@@ -67,6 +69,7 @@ impl MDBook {
             description: String::new(),
 
             content: vec![],
+            books: HashMap::new(),
             renderer: Box::new(HtmlHandlebars::new()),
 
             livereload: None,
