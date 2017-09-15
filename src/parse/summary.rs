bb12d6b10c476c0984af4cd9dc0b4b4e8fc2226e
Pratik Karki <prertik@outlook.com>
2017-09-15 20:18:09 +0545

    rustfmt the repository #398

--- src/parse/summary.rs
+++ src/parse/summary.rs
@@ -60,7 +60,8 @@ fn parse_level(
                                        Suffix and Spacer elements can only exist \
                                        on the root level.\n
                         \
-                                       Prefix elements can only exist before any chapter and there can be \
+                                       Prefix elements can only exist before \
+                                       any chapter and there can be \
                                        no chapters after suffix elements.",
                 ));
             };
