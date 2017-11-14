5dbd4a5edf37cb84b23b5a4e9af8e8729065f569
Jacob Wahlgren <jacob.wahlgren@gmail.com>
2017-11-14 17:06:49 +0100

    Refactor toc::link

--- src/renderer/html_handlebars/helpers/toc.rs
+++ src/renderer/html_handlebars/helpers/toc.rs
@@ -21,26 +21,25 @@ fn active_link(rc: &RenderContext) -> Result<String, RenderError> {
 pub fn link(h: &Helper, r: &Handlebars, rc: &mut RenderContext) -> Result<(), RenderError> {
     let link = rc.evaluate("link")?.clone();
 
-    let has_link = if let Some(link) = link.as_str() {
+    if let Some(link) = link.as_str() {
         rc.writer.write_all(b"<a")?;
 
-        if active_link(rc)? == link {
+        let active = active_link(rc)?;
+
+        if active == link {
             rc.writer.write_all(b" class=\"active\"")?;
         }
 
         rc.writer.write_all(b" href=\"")?;
         rc.writer.write_all(link.as_bytes())?;
         rc.writer.write_all(b"\">")?;
-        true
-    } else {
-        false
-    };
+    }
 
     h.template()
         .ok_or_else(|| RenderError::new("No template for link"))
         .and_then(|t| t.render(r, rc))?;
 
-    if has_link {
+    if link.is_string() {
         rc.writer.write_all(b"</a>")?;
     }
 
