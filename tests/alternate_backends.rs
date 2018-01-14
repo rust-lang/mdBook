87c4c17efadf7efaebe28747b4e1b8f9dc1e34fd
Michael Bryan <michaelfbryan@gmail.com>
2018-01-14 18:02:55 +0800

    Made it so the CmdRenderer writes directly to the child's stdin

--- tests/alternate_backends.rs
+++ tests/alternate_backends.rs
@@ -3,9 +3,11 @@
 extern crate mdbook;
 extern crate tempdir;
 
+use std::fs::File;
 use tempdir::TempDir;
 use mdbook::config::Config;
 use mdbook::MDBook;
+use mdbook::renderer::RenderContext;
 
 #[test]
 fn passing_alternate_backend() {
@@ -26,6 +28,22 @@ fn alternate_backend_with_arguments() {
     let (md, _temp) = dummy_book_with_backend("arguments", "echo Hello World!");
 
     md.build().unwrap();
+}
+
+#[test]
+fn backends_receive_render_context_via_stdin() {
+    let temp = TempDir::new("output").unwrap();
+    let out_file = temp.path().join("out.txt");
+    let cmd = format!("tee {}", out_file.display());
+
+    let (md, _temp) = dummy_book_with_backend("cat-to-file", &cmd);
+
+    assert!(!out_file.exists());
+    md.build().unwrap();
+    assert!(out_file.exists());
+
+    let got = RenderContext::from_json(File::open(&out_file).unwrap());
+    assert!(got.is_ok());
 }
 
 fn dummy_book_with_backend(name: &str, command: &str) -> (MDBook, TempDir) {
