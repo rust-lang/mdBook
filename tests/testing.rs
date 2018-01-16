4177288b11ba5c965a274e7377bc45c41faef713
Jaime Valdemoros <jaimevg1123@gmail.com>
2018-01-16 22:13:47 +0000

    Add test to make sure pre-processors are being run

--- tests/testing.rs
+++ tests/testing.rs
@@ -1,10 +1,18 @@
 extern crate mdbook;
+#[macro_use]
+extern crate lazy_static;
 
 mod dummy_book;
 
 use dummy_book::DummyBook;
-use mdbook::MDBook;
 
+use mdbook::MDBook;
+use mdbook::preprocess::{Preprocessor, PreprocessorContext};
+use mdbook::book::Book;
+use mdbook::config::Config;
+use mdbook::errors::*;
+
+use std::sync::Mutex;
 
 #[test]
 fn mdbook_can_correctly_test_a_passing_book() {
@@ -20,4 +28,34 @@ fn mdbook_detects_book_with_failing_tests() {
     let mut md: MDBook = MDBook::load(temp.path()).unwrap();
 
     assert!(md.test(vec![]).is_err());
+}
+
+#[test]
+fn mdbook_runs_preprocessors() {
+
+    lazy_static! {
+        static ref HAS_RUN: Mutex<bool> = Mutex::new(false);
+    }
+
+    struct DummyPreprocessor;
+
+    impl Preprocessor for DummyPreprocessor {
+        fn name(&self) -> &str {
+            "dummy"
+        }
+
+        fn run(&self, _ctx: &PreprocessorContext, _book: &mut Book) -> Result<()> {
+            *HAS_RUN.lock().unwrap() = true;
+            Ok(())
+        }
+    }
+
+    let temp = DummyBook::new().build().unwrap();
+    let cfg = Config::default();
+
+    let mut book = MDBook::load_with_config(temp.path(), cfg).unwrap();
+    book.with_preprecessor(DummyPreprocessor);
+    book.build().unwrap();
+
+    assert!(*HAS_RUN.lock().unwrap())
 }
