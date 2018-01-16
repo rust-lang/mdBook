60380d3eac2c570f4b019a8e6e2157a095832d3f
Michael Bryan <michaelfbryan@gmail.com>
2018-01-16 21:33:11 +0800

    Made sure travis checks mdbook-wordcount

--- ci/script.sh
+++ ci/script.sh
@@ -2,9 +2,9 @@
 
 set -ex
 
-# TODO This is the "test phase", tweak it as you see fit
 main() {
     cross build --target $TARGET
+    cross build --manifest-path ./book-example/src/for_developers/mdbook-wordcount/Cargo.toml
     cross build --target $TARGET --release
 
     if [ ! -z $DISABLE_TESTS ]; then
