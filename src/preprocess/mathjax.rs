643edbe87aa67ef4e2392208d0e55934f79e63a3
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-02-17 11:52:44 +0100

    Create a skeletal NOOP implementation of MathJaxPreprocessor

--- src/preprocess/mathjax.rs
+++ src/preprocess/mathjax.rs
@@ -3,3 +3,28 @@
 //! This preprocessor takes inline expressions wrapped in `$`-pairs and block
 //! expressions wrapped in `$$`-pairs and transform them into a valid MathJax
 //! expression that does not interfere with the markdown parser.
+
+use errors::Result;
+
+use super::{Preprocessor, PreprocessorContext};
+use book::Book;
+
+/// a preprocessor for expanding `$`- and `$$`-pairs into valid MathJax expressions.
+pub struct MathJaxPreprocessor;
+
+impl MathJaxPreprocessor {
+    /// Create a `MathJaxPreprocessor`.
+    pub fn new() -> Self {
+        MathJaxPreprocessor
+    }
+}
+
+impl Preprocessor for MathJaxPreprocessor {
+    fn name(&self) -> &str {
+        "mathjax"
+    }
+
+    fn run(&self, _ctx: &PreprocessorContext, _book: &mut Book) -> Result<()> {
+        Ok(())
+    }
+}
