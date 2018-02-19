784b814332c00131d55f49455db31c2951351e05
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2018-02-19 14:13:25 +0100

    Implement find_mathematics for inline maths

--- src/preprocess/mathjax.rs
+++ src/preprocess/mathjax.rs
@@ -5,6 +5,7 @@
 //! expression that does not interfere with the markdown parser.
 
 use errors::Result;
+use regex::{CaptureMatches, Captures, Regex};
 
 use super::{Preprocessor, PreprocessorContext};
 use book::{Book, BookItem};
@@ -42,7 +43,7 @@ fn replace_all_mathematics(content: &str) -> String {
 
     for math in find_mathematics(content) {
         replaced.push_str(&content[previous_end_index..math.start_index]);
-        replaced.push_str(math.replacement());
+        replaced.push_str(&math.replacement());
         previous_end_index = math.end_index;
     }
 
@@ -51,26 +52,40 @@ fn replace_all_mathematics(content: &str) -> String {
     replaced
 }
 
-fn find_mathematics(_content: &str) -> MathematicsIterator {
-    MathematicsIterator
+fn find_mathematics(content: &str) -> MathematicsIterator {
+    lazy_static! {
+        static ref REGEXP: Regex = Regex::new(r"(?x) # insignificant whitespace mode
+            \$    # a dollar sign
+            [^$]+ # followed by one or more things other than a dollar sign
+            \$    # followed by a dollar sign.
+        ").unwrap();
+    }
+    MathematicsIterator(REGEXP.captures_iter(content))
 }
 
-struct MathematicsIterator;
+struct MathematicsIterator<'a>(CaptureMatches<'a, 'a>);
 
-impl Iterator for MathematicsIterator {
+impl<'a> Iterator for MathematicsIterator<'a> {
     type Item = Mathematics;
 
     fn next(&mut self) -> Option<Self::Item> {
+        for capture in &mut self.0 {
+            if let mathematics @ Some(_) = Mathematics::from_capture(capture) {
+                return mathematics;
+            }
+        }
         None
     }
 }
 
+#[derive(Debug, PartialEq, Eq)]
 struct Mathematics {
     start_index: usize,
     end_index: usize,
     kind: Kind,
 }
 
+#[derive(Debug, PartialEq, Eq)]
 enum Kind {
     Inline,
     Block,
@@ -79,7 +94,40 @@ enum Kind {
 }
 
 impl Mathematics {
+    fn from_capture<'a>(captures: Captures<'a>) -> Option<Self> {
+        captures.get(0).map(|m| Mathematics {
+            start_index: m.start(),
+            end_index: m.end(),
+            kind: Kind::Inline,
+        })
+    }
+
     fn replacement(&self) -> String {
         unimplemented!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_find_no_mathematics_in_regular_text() {
+        let content = "Text without mathematics";
+
+        assert_eq!(find_mathematics(content).count(), 0);
+    }
+
+    #[test]
+    fn should_find_inline_mathematics() {
+        let content = "Pythagorean theorem: $a^{2} + b^{2} = c^{2}$";
+
+        let result = find_mathematics(content).collect::<Vec<_>>();
+        assert_eq!(result.len(), 1);
+        assert_eq!(result[0], Mathematics {
+            start_index: 21,
+            end_index: 44,
+            kind: Kind::Inline,
+        })
     }
 }
