38c06f3c391d140cc81fbd248051fa659520695f
Michael Bryan <michaelfbryan@gmail.com>
2018-04-07 06:15:26 +0800

    Removed all the unnecessary CI jobs

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -6,20 +6,9 @@ main() {
     local src=$(pwd) \
           stage=
 
-    case $TRAVIS_OS_NAME in
-        linux)
-            stage=$(mktemp -d)
-            ;;
-        osx)
-            stage=$(mktemp -d -t tmp)
-            ;;
-    esac
+    cargo rustc --bin mdbook --target $TARGET --release -- -C lto
 
-    test -f Cargo.lock || cargo generate-lockfile
-
-    cross rustc --bin mdbook --target $TARGET --release -- -C lto
-
-    cp target/$TARGET/release/mdbook $stage/
+    cp target/release/mdbook $stage/
 
     cd $stage
     tar czf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.gz *
