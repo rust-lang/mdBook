b3665c287dd49e973434d650ebd39708f4fd5469
Matt Ickstadt <mattico8@gmail.com>
2018-08-01 17:59:40 -0500

    Add directory argument to `mdbook test`

--- src/cmd/test.rs
+++ src/cmd/test.rs
@@ -7,7 +7,10 @@ use mdbook::MDBook;
 pub fn make_subcommand<'a, 'b>() -> App<'a, 'b> {
     SubCommand::with_name("test")
         .about("Test that code samples compile")
-        .arg_from_usage("-L, --library-path [DIR]... 'directory to add to crate search path'")
+        .arg_from_usage("-L, --library-path [DIR]... 'directories to add to crate search path'")
+        .arg_from_usage(
+            "[dir] 'A directory for your book{n}(Defaults to Current Directory when omitted)'",
+        )
 }
 
 // test command implementation
