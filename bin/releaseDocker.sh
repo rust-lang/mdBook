e00749d9c5b8e5d47e8670b67805183d0f790449
Miroslav Kravec <kravec.miroslav@gmail.com>
2018-12-08 10:54:44 +0100

    Add releaseDocker.sh

---
+++ bin/releaseDocker.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# USAGE: ./bin/releaseDocker.sh <ACCOUNT_NAME> <VERSION>
+#
+# Example usage: ./bin/releaseDocker.sh dockeruser 0.2.1
+
+set -e
+
+SCRIPT_DIRECTORY="$(dirname "$0")"
+PROJECT_DIRECTORY="$(dirname "${SCRIPT_DIRECTORY}")"
+
+cd "${PROJECT_DIRECTORY}"
+
+docker build --no-cache  -t mdbook .
+docker tag mdbook "${1}/mdbook:${2}"
+
+echo "To run image:"
+echo docker run -it mdbook sh
+
+echo "To push (publish) image"
+echo docker push "${1}/mdbook:${2}"
+
