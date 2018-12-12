3d8db7f25c8b0a8760d88200a973f25fa10f79c1
gentoo90 <gentoo90@gmail.com>
2018-12-12 21:43:02 +0200

    Disable text selection and CSS transition while resizing sidebar

--- src/theme/book.js
+++ src/theme/book.js
@@ -480,12 +480,14 @@ function playpen_text(playpen) {
     function initResize(e) {
         window.addEventListener('mousemove', resize, false);
         window.addEventListener('mouseup', stopResize, false);
+        html.classList.add('sidebar-resizing');
     }
     function resize(e) {
         document.documentElement.style.setProperty('--sidebar-width', (e.clientX - sidebar.offsetLeft) + 'px');
     }
     //on mouseup remove windows functions mousemove & mouseup
     function stopResize(e) {
+        html.classList.remove('sidebar-resizing');
         window.removeEventListener('mousemove', resize, false);
         window.removeEventListener('mouseup', stopResize, false);
     }
