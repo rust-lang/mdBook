43ead86ecc466f6be9fdd6eb9e573dc53cf28c18
Eric Huss <eric@huss.org>
2019-05-31 09:00:15 -0700

    Update toml. (#945)
    Just keeping up-to-date.

--- src/config.rs
+++ src/config.rs
@@ -206,7 +206,7 @@ impl Config {
         } else if index.starts_with("build.") {
             self.build.update_value(&index[6..], value);
         } else {
-            self.rest.insert(index, value)?;
+            self.rest.insert(index, value).map_err(|e| ErrorKind::TomlQueryError(e))?;
         }
 
         Ok(())
