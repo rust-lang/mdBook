831d7f162c63b7a9969379c632d8010b1b1cddb6
Ruben Moor <ruben.moor@gmail.com>
2020-07-17 20:28:50 -0500

    local nix package

--- default.nix
+++ default.nix
@@ -1,7 +1,3 @@
 { pkgs }:
-pkgs.rustPlatform.buildRustPackage rec {
-  pname = "mdbook-${version}";
-  version = "0.4.2";
-  src = ./.;
-  cargoSha256 = "0ch25ivww62j2kwsd20159hwjfyhfarwmqy9hqn1lv2vhfskry2b";
-}
+let cargo_nix = pkgs.callPackage ./Cargo.nix {};
+in cargo_nix.rootCrate.build
