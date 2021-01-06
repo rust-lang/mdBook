d2b8a8273f1c3c618ece1467263437905b526d8e
Eric Huss <eric@huss.org>
2021-01-06 14:28:26 -0800

    Change `init --theme` to place theme in root.

--- tests/init.rs
+++ tests/init.rs
@@ -108,3 +108,37 @@ fn book_toml_isnt_required() {
 
     md.build().unwrap();
 }
+
+#[test]
+fn copy_theme() {
+    let temp = TempFileBuilder::new().prefix("mdbook").tempdir().unwrap();
+    MDBook::init(temp.path()).copy_theme(true).build().unwrap();
+    let expected = vec![
+        "book.js",
+        "css/chrome.css",
+        "css/general.css",
+        "css/print.css",
+        "css/variables.css",
+        "favicon.png",
+        "favicon.svg",
+        "highlight.css",
+        "highlight.js",
+        "index.hbs",
+    ];
+    let theme_dir = temp.path().join("theme");
+    let mut actual: Vec<_> = walkdir::WalkDir::new(&theme_dir)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| !e.file_type().is_dir())
+        .map(|e| {
+            e.path()
+                .strip_prefix(&theme_dir)
+                .unwrap()
+                .to_str()
+                .unwrap()
+                .replace('\\', "/")
+        })
+        .collect();
+    actual.sort();
+    assert_eq!(actual, expected);
+}
