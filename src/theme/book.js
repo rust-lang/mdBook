68d8ceec47850a3ee74702cdec36f4c2aaf4be0f
Eric Huss <eric@huss.org>
2021-08-02 08:02:13 -0700

    Revert #1597 - Update to highlight.js 11.0.

--- src/theme/book.js
+++ src/theme/book.js
@@ -169,9 +169,9 @@ function playground_text(playground) {
         Array
         code_nodes
             .filter(function (node) {return !node.classList.contains("editable"); })
-            .forEach(function (block) { hljs.highlightElement(block); });
+            .forEach(function (block) { hljs.highlightBlock(block); });
     } else {
-        code_nodes.forEach(function (block) { hljs.highlightElement(block); });
+        code_nodes.forEach(function (block) { hljs.highlightBlock(block); });
     }
 
     // Adding the hljs class gives code blocks the color css
