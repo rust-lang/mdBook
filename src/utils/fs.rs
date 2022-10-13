f144b57850c5325207a8ce5676788578b0bbbaff
Bergmann89 <info@bergmann89.de>
2022-10-13 15:55:16 +0200

    Ignore files listed in `.mdbookignore` during build

--- src/utils/fs.rs
+++ src/utils/fs.rs
@@ -1,4 +1,5 @@
 use crate::errors::*;
+use ignore::gitignore::Gitignore;
 use log::{debug, trace};
 use std::convert::Into;
 use std::fs::{self, File};
@@ -94,13 +95,13 @@ pub fn copy_files_except_ext(
     to: &Path,
     recursive: bool,
     avoid_dir: Option<&PathBuf>,
-    ext_blacklist: &[&str],
+    ignore: Option<&Gitignore>,
 ) -> Result<()> {
     debug!(
         "Copying all files from {} to {} (blacklist: {:?}), avoiding {:?}",
         from.display(),
         to.display(),
-        ext_blacklist,
+        ignore,
         avoid_dir
     );
 
@@ -128,6 +129,13 @@ pub fn copy_files_except_ext(
                 }
             }
 
+            if let Some(ignore) = ignore {
+                let path = entry.path();
+                if ignore.matched(&path, path.is_dir()).is_ignore() {
+                    continue;
+                }
+            }
+
             // check if output dir already exists
             if !to.join(entry.file_name()).exists() {
                 fs::create_dir(&to.join(entry.file_name()))?;
@@ -138,15 +146,17 @@ pub fn copy_files_except_ext(
                 &to.join(entry.file_name()),
                 true,
                 avoid_dir,
-                ext_blacklist,
+                ignore,
             )?;
         } else if metadata.is_file() {
             // Check if it is in the blacklist
-            if let Some(ext) = entry.path().extension() {
-                if ext_blacklist.contains(&ext.to_str().unwrap()) {
+            if let Some(ignore) = ignore {
+                let path = entry.path();
+                if ignore.matched(&path, path.is_dir()).is_ignore() {
                     continue;
                 }
             }
+
             debug!(
                 "creating path for file: {:?}",
                 &to.join(
@@ -191,6 +201,7 @@ pub fn get_404_output_file(input_404: &Option<String>) -> String {
 #[cfg(test)]
 mod tests {
     use super::copy_files_except_ext;
+    use ignore::gitignore::GitignoreBuilder;
     use std::{fs, io::Result, path::Path};
 
     #[cfg(target_os = "windows")]
@@ -247,9 +258,19 @@ mod tests {
             panic!("Could not create output/sub_dir_exists: {}", err);
         }
 
-        if let Err(e) =
-            copy_files_except_ext(tmp.path(), &tmp.path().join("output"), true, None, &["md"])
-        {
+        let ignore = GitignoreBuilder::new(tmp.path())
+            .add_line(None, "*.md")
+            .expect("Unable to add '*.md' to gitignore builder")
+            .build()
+            .expect("Unable to build gitignore");
+
+        if let Err(e) = copy_files_except_ext(
+            tmp.path(),
+            &tmp.path().join("output"),
+            true,
+            None,
+            Some(&ignore),
+        ) {
             panic!("Error while executing the function:\n{:?}", e);
         }
 
