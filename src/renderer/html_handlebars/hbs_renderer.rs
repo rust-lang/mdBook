96eab23c72e946cecb45a850785d7806eb7b7a8e
Jooyung Han <jooyung@google.com>
2023-02-22 15:30:42 +0900

    Pass `translations` to HTML renderer

--- src/renderer/html_handlebars/hbs_renderer.rs
+++ src/renderer/html_handlebars/hbs_renderer.rs
@@ -635,6 +635,12 @@ fn make_data(
         "language".to_owned(),
         json!(config.book.language.clone().unwrap_or_default()),
     );
+    if !config.book.translations.is_empty() {
+        data.insert(
+            "translations".to_owned(),
+            json!(config.book.translations),
+        );
+    }
     data.insert(
         "book_title".to_owned(),
         json!(config.book.title.clone().unwrap_or_default()),
