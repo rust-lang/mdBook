e42118057ed65a41d83e260b6b65dc4592ee670f
Robert Hensing <robert@roberthensing.nl>
2025-05-15 14:51:49 +0200

    Add canonical-site-url setting
    Based on the conversation in https://github.com/rust-lang/mdBook/issues/1238,
    this implements the suggestion by markhildreth to implement such a setting.
    
    I've additionally infixed `-site-` to highlight the relationship with the
    `site-url`, and to distinguish it from the canonical URL as it occurs in a
    page.

--- tests/testsuite/rendering.rs
+++ tests/testsuite/rendering.rs
@@ -41,3 +41,25 @@ fn first_chapter_is_copied_as_index_even_if_not_first_elem() {
             ]],
         );
 }
+
+// Checks that a canonical URL is generated correctly.
+#[test]
+fn canonical_url() {
+    BookTest::from_dir("rendering/canonical_url")
+        .check_file_contains(
+            "book/index.html",
+            "<link rel=\"canonical\" href=\"https://example.com/test/\">",
+        )
+        .check_file_contains(
+            "book/canonical_url.html",
+            "<link rel=\"canonical\" href=\"https://example.com/test/canonical_url.html\">",
+        )
+        .check_file_contains(
+            "book/nested/page.html",
+            "<link rel=\"canonical\" href=\"https://example.com/test/nested/page.html\">",
+        )
+        .check_file_contains(
+            "book/nested/index.html",
+            "<link rel=\"canonical\" href=\"https://example.com/test/nested/\">",
+        );
+}
