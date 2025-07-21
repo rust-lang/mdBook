461884f10928c18233b7ecba39008a54cbe198e7
Eric Huss <eric@huss.org>
2025-07-21 11:07:57 -0700

    Add mdbook-preprocessor and mdbook-renderer
    These are two new crates intended to support implementing preprocessors
    and renderers. Currently these stubs just have MDBOOK_VERSION, but
    future commits will migrate more code to these crates.

---
+++ crates/mdbook-preprocessor/src/lib.rs
@@ -0,0 +1,3 @@
+//! Library to assist implementing an mdbook preprocessor.
+
+pub use mdbook_core::MDBOOK_VERSION;
---
+++ crates/mdbook-renderer/src/lib.rs
@@ -0,0 +1,3 @@
+//! Library to assist implementing an mdbook renderer.
+
+pub use mdbook_core::MDBOOK_VERSION;
