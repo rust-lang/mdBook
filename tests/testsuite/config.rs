f6c11d12e050d1163e5dc9eba7ee76c4c1547ddd
Gabor Szabo <gabor@szabgab.com>
2025-08-24 09:49:43 +0300

    add test: invalid table at the top level config

--- tests/testsuite/config.rs
+++ tests/testsuite/config.rs
@@ -132,6 +132,31 @@ unknown field `foo`, expected one of `book`, `build`, `rust`, `output`, `preproc
         });
 }
 
+// An invalid table at the top level.
+#[test]
+fn bad_config_top_level_table() {
+    BookTest::init(|_| {})
+        .change_file(
+            "book.toml",
+            "[other]\n\
+            foo = 123",
+        )
+        .run("build", |cmd| {
+            cmd.expect_failure()
+                .expect_stdout(str![[""]])
+                .expect_stderr(str![[r#"
+[TIMESTAMP] [ERROR] (mdbook_core::utils): Error: Invalid configuration file
+[TIMESTAMP] [ERROR] (mdbook_core::utils): [TAB]Caused By: TOML parse error at line 1, column 2
+  |
+1 | [other]
+  |  ^^^^^
+unknown field `other`, expected one of `book`, `build`, `rust`, `output`, `preprocessor`
+
+
+"#]]);
+        });
+}
+
 // An invalid key in the main book table.
 #[test]
 fn bad_config_in_book_table() {
