258cce2b43b463d388c7c8bc25cf1fe4a99d7dd7
Chocorean <chocorean@pm.me>
2026-09-10 19:17:00 -0400

    move test case

--- tests/testsuite/sidebar_logo.rs
+++ tests/testsuite/sidebar_logo.rs
@@ -2,7 +2,7 @@ use crate::prelude::*;
 
 #[test]
 fn bad_logo_path() {
-    BookTest::from_dir("sidebar/logo").run("build", |cmd| {
+    BookTest::from_dir("sidebar/logo/not_found").run("build", |cmd| {
         cmd.expect_failure();
         cmd.expect_stderr(
             "ERROR invalid value for `logo`: [ROOT]/src/does/not/exist does not exist\n",
